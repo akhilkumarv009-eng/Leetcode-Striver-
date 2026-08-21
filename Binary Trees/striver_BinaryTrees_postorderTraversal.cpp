c8e0fa4a25c3fdf71ce482b4e76fc4bda9b6450e
Akhil <akhilkumarv009@gmail.com>
2026-08-21 19:27:52 +0530

    Solved trees  Traversal  problem

---
+++ Binary Trees/striver_BinaryTrees_postorderTraversal.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x)
+    {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    void preorder(TreeNode* root, vector<int> &ans)
+    {
+        if (root == NULL)
+            return;
+        
+        preorder(root->left, ans);
+        ans.push_back(root->val);
+
+        preorder(root->right, ans);
+    }
+
+    vector<int> preorderTraversal(TreeNode* root)
+    {
+        vector<int> ans;
+        preorder(root, ans);
+        return ans;
+    }
+};
+
+int main()
+{
+    // Creating the binary tree
+    //
+    //        5
+    //       / \
+    //      3   4
+    //
+    TreeNode* root = new TreeNode(5);
+    root->left = new TreeNode(3);
+    root->right = new TreeNode(4);
+
+    Solution obj;
+
+    vector<int> result = obj.preorderTraversal(root);
+
+    // Print preorder traversal
+    for (int x : result)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}
