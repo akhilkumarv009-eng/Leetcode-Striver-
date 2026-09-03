4f3901221005fd08cb4bd4aca88a8de10a95be8f
Akhil <akhilkumarv009@gmail.com>
2026-09-03 22:59:14 +0530

    Solved tree traversal problem

---
+++ arrays/striver_arrays_fibbonaci.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int fib (int n)
+{
+    if(n==0)return 0;
+    if(n==1)return 1;
+    else
+    return fib(n-1)+fib(n-2);
+}
+int main()
+{
+    int ans = fib(3);
+    cout << ans;
+}
