2c8ac07b71b340aae3cd1d3e2c6a7074f4622916
Akhil <akhilkumarv009@gmail.com>
2026-09-10 23:07:27 +0530

    Solved tree traversal problem

---
+++ Daily hunt/10-9-26.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+class Solution {
+public:
+    int ans = 0;
+
+    pair<int, int> dfs(TreeNode* root) {
+        if (root == NULL)
+            return {0, 0};
+
+        pair<int, int> left = dfs(root->left);
+        pair<int, int> right = dfs(root->right);
+
+        int sum = left.first + right.first + root->val;
+        int count = left.second + right.second + 1;
+
+        if (sum / count == root->val)
+            ans++;
+
+        return {sum, count};
+    }
+
+    int averageOfSubtree(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
